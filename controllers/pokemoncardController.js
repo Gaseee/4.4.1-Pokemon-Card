61067fbacfaaf2f6f3aed202a141cd353a92a172
ll3m <ll3m@fresnou.org>
2021-12-06 08:59:48 -0800

    completed two cards but still need to modify and refine my cards a bit

---
+++ controllers/pokemoncardController.js
@@ -0,0 +1,19 @@
+module.exports.viewAll = function(req,res,next){
+    const pokemoncard = {
+        id: 1,
+        name: 'Pikachu',
+        health: '50 HP',
+        type: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg',
+        pokemonPicture: 'https://cdn.ndtv.com/tech/images/gadgets/pikachu_hi_pokemon.jpg',
+        energy1: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg',
+        move1: 'Gnaw',
+        number1: '10',
+        energy2: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg',
+        move2: 'Lighting Bolt',
+        number2: '',
+        weaknessImg: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg',
+        resistanceImg: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg',
+        retreatCostImg: 'http://pm1.narvii.com/5867/6c6a05dde0693c1dfe454912c83109f165f3f7ab_00.jpg'
+
+    }
+}
