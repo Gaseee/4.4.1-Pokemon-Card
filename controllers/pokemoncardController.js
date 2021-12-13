339c80573126fdbb87bebf1b69666e37786599ea
ll3m <ll3m@fresnou.org>
2021-12-13 08:54:33 -0800

    On part 4, slide 31, edit button not working

--- controllers/pokemoncardController.js
+++ controllers/pokemoncardController.js
@@ -1,5 +1,39 @@
 const {pokemonCard} = require('../models')
+
 module.exports.viewAll = async function(req, res, next) {
     const pokemoncards = await pokemonCard.findAll();
     res.render('index', {pokemoncards});
+};
+
+module.exports.renderEditForm = async function(req, res, next) {
+    const pokemoncard = await pokemonCard.findByPK(
+        req.params.id
+    );
+    res.render('edit', {pokemoncard});
+};
+
+module.exports.updatePokemonCard = async function(req,res){
+    await pokemonCard.update(
+        {
+            name: req.body.name,
+            health: req.body.health,
+            type: req.body.type,
+            pokemonPicture: req.body.pokemonPicture,
+            energy1: req.body.energy1,
+            move1: req.body.move1,
+            number1: req.body.number1,
+            energy2: req.body.energy2,
+            move2: req.body.move2,
+            number2: req.body.number2,
+            weaknessImg: req.body.weaknessImg,
+            resistanceImg: req.body.resistanceImg,
+            retreatCostImg: req.body.retreatCostImg
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const pokemoncardController = require('../controllers/pokemoncardController');
 
 /* GET home page. */
 router.get('/', pokemoncardController.viewAll);
+router.get('/edit/:id', pokemoncardController.renderEditForm);
+router.post('/edit/:id', pokemoncardController.updatePokemonCard);
+
+
 
 module.exports = router;
